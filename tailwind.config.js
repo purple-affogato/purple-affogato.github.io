8e7b32a906f11c426ff6b42bccaa284debd71ca9
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-07-12 17:48:25 -0400

    cries ;-;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
   theme: {
+    extend: {},
+    colors: {
+      'light-purple' : '#c9adf0',
+      'medium-purple' : '#8034eb',
+      'dark-purple' : '#40078f'
+    },
+    fontFamily: {
+      'sans' : ['Impact', 'sans-serif']
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
