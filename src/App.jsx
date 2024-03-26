2760d710bc18673a93b6433b59550bd157e408f0
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-03-26 01:45:40 -0400

    moved to vite

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+import ErrorPage from './Error.jsx';
+import Homepage from './Homepage.jsx';
+import ProjectList from './Projects.jsx';
+import Template from './Template.jsx';
+import AboutMe from './AboutMe.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+            <Route path="/" element={<Template />} errorElement={<ErrorPage/>}>
+                <Route index element={<Homepage/>}  />
+                <Route path='*' element={<Error/>} />
+                <Route path="about-me" element={<AboutMe />}/>
+                <Route path="projects" element={<ProjectList />}/>
+            </Route>
+        </Routes>
+        
+    </>
+  )
+}
+
+export default App
