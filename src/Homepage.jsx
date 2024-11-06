cf735971118874d397596054dc71df2a067c5fd0
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-11-05 18:50:47 -0800

    Adjusted contacts and links on home page

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -7,8 +7,9 @@ function Homepage() {
         <h1>Nice to meet u :)</h1>
         <p>You probably know me as purple-affogato (or other names irl!), but that's what I go by for CS and developer-related stuff. </p>
         <p>While you're here though, check out my projects and learn more about me. :D</p>
-        <Link to="https://github.com/purple-affogato" className="link">My GitHub</Link>
-        <Link to="https://purple-affogato.itch.io/" className="link">My Itch.io Profile</Link>
+        <h1>My Contacts</h1>
+        <Link to="https://github.com/purple-affogato" className="link">GitHub</Link>
+        <Link to="https://www.linkedin.com/in/ivy-y-zhuang" className="link">LinkedIn</Link>
     </div>
 );
 }
