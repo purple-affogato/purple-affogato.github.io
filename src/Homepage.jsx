e7dae69da868b5d14bc4bd64afcb27dc971e3371
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-11-07 19:29:33 -0800

    Slight changes in website colors

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -5,8 +5,10 @@ function Homepage() {
   return (
     <div>
         <h1>Nice to meet u :)</h1>
-        <p>You probably know me as purple-affogato (or other names irl!), but that's what I go by for CS and developer-related stuff. </p>
+        <p>You probably know me as purple-affogato (or other names irl!), but that's what I go by on GitHub. </p>
         <p>While you're here though, check out my projects and learn more about me. :D</p>
+        <h1>Skills</h1>
+        
         <h1>My Contacts</h1>
         <Link to="https://github.com/purple-affogato" className="link">GitHub</Link>
         <Link to="https://www.linkedin.com/in/ivy-y-zhuang" className="link">LinkedIn</Link>
--- src/Project.jsx
+++ src/Project.jsx
@@ -2,7 +2,7 @@ function Project({title, description, tags, github_link}){
     return (
         <div>
             <h1>{title}</h1>
-            <p>{descriptsion}</p>
+            <p>{description}</p>
             <Link to={github_link.toString()}>GitHub Repo</Link>
         </div>
     );
