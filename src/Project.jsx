3261f5246978de0b2016838182f99faf6e45f82f
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-11-10 15:23:23 -0800

    Added project list and adjusted styling again

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+import './Project.css';
+
 function Project({title, description, tags, github_link}){
+    const points = [];
+    for (let i=0;i<description.length;i++){
+            points.push(<li key={i}>{description[i].toString()}</li>);
+    }
     return (
         <div>
-            <h1>{title}</h1>
-            <p>{description}</p>
-            <Link to={github_link.toString()}>GitHub Repo</Link>
+            <h2>{title}</h2>
+            <>{points}</>
+            <Link to={github_link.toString()} className="github">GitHub Repo</Link>
         </div>
     );
 }
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import Project from  "./Project.jsx";
+import { projects } from "./ProjectsData.jsx";
 
 function ProjectList() {
-    useEffect(() => {
-
-    });
+    const listItems = projects.map((project) => (
+        <Project key={project.id} title={project.title} description={project.description} github_link={project.github_link}/>
+    ));
     return(
         <div>
             <h1>Projects</h1>
-            <p>i will soon list my projects here :)</p>
+            <p>Sorted in chronological order from most recent to earliest.</p>
+            <>{listItems}</>
         </div>
     );
 }
