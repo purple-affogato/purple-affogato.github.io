a54a18b23c0beb9b2cd783cb30c3236c5dbfd291
purple-affogato <ivy.yiting.zhuang@gmail.com>
2025-02-04 14:46:27 -0800

    Updated w/ more recent info

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -3,11 +3,11 @@ function AboutMe (){
     return (
         <div>
             <h1>About Me</h1>
-            <p>Hi! Right now, I am a university student at Santa Clara University majoring in Computer Science and Engineering (CSEN). Anyways, here are a few things about me. :&gt;</p>
-            <p>Hobbies: reading manga, listening to kpop, photography, coffee & tea</p>
+            <p>Hi! Right now, I am a university student at Santa Clara University majoring in Computer Science and Engineering (CSEN). I'm a part of ACM and SWE++ at SCU. </p>
+            <p>Here are a few things about me. :&gt;</p>
+            <p>Hobbies: reading manga, photography, coffee & tea</p>
             <p>Favourite Food: suan cai yu, crab meat & fish maw soup, wonton noodles from Jim Chai Kee, congee w/ century egg</p>
-            <p>Favourite Music: IVE, Heize, aespa</p>
-            <p>Currently playing: PATH TO NOWHERE</p>
+            <p>Favourite Music Artists: IVE, Heize, aespa, Jackie Cheung, Priscilla Chan, Path to Nowhere</p>
         </div>
     )
 }
